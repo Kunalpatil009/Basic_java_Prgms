0e780edea16856f2d1daef0ed290d2268c94e42c
Kunalpatil009 <009kunalpatil@gmail.com>
2022-06-27 22:30:05 +0530

    Prb_3_Solution

---
+++ Prb_3_HarmonicNo.java
@@ -0,0 +1,21 @@
+package Java_Basic_Progms;
+import java.util.Scanner;
+
+public class Prb_3_HarmonicNo {
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+        System.out.println("Enter the Number");
+        int number = num.nextInt();
+        double nthHarmonic = 0.0;
+        if (number != 0) {
+            for (int i = 1; i <= number; ++i) {
+                System.out.println("1/" + i ) ;
+                nthHarmonic += 1.0 / i;
+                    }
+                }
+                else {
+                    System.out.println("Invalid Input Enter the Non-Zero Input");
+                }
+                System.out.println("=" + nthHarmonic);
+    }
+}
